ad3ac949c140024a2a0205384c2d5c7aacbf2ae3
Kishan Modi <kishandmodi@gmail.com>
2024-06-22 15:21:17 -0400

    Tailwind Initial Setup For Frontend

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
+
+import Loader from './common/Loader';
+import PageTitle from './components/PageTitle';
+
+function App() {
+    const [loading, setLoading] = useState<boolean>(true);
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
+    useEffect(() => {
+        setTimeout(() => setLoading(false), 1000);
+    }, []);
+
+    return loading ? (
+        <Loader />
+    ) : (
+        <>
+            <Routes>
+            </Routes>
+        </>
+    );
+}
+
+export default App;
